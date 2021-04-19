79a67139aa2a2a7b9c66ff39be29ea0ecc2e5b9c
Alina Nenasheva <alinochka.no1@yandex.ru>
2021-04-19 19:46:39 +0300

    add ClientRequestCustomHeader, HTTPBody(init and message-forming methods) classes, parse commandlines args func

--- setLab1.py
+++ setLab1.py
@@ -1,10 +1,26 @@
 import socket
+import argparse
+from string import Template
+import http-parser
+
+parser_arg = argparse.AgrumentParser()
+http_parser = HttpParser()
+
+def init_parse_args():
+    parser_arg.add_argument('h', 'host', type=str, help='this is port')
+    parser_arg.add_argument('a', 'accept', help='accepted type of content')
+    parser_arg.add_argument('cl', 'content_length', help='the length of content')
+    parser_arg.add_argument('al', 'accept_language', help='the language of content')
+    args = parser_arg.parse_args()
+    return args
+   
 
 class ServerAdress:
 
     def __init__(self, host='localhost', port=8000):
        self.host = host
        self.port = port
+
      
 class ClientSocket:
 
@@ -13,7 +29,8 @@ class ClientSocket:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
-          
+        
+    
 
     def connect(self, server_adress):
         self.sock.connect((server_adress.host, server_adress.port)) 
@@ -26,15 +43,35 @@ class ClientSocket:
     def receive(self):
         response = ''
         while True:
-            recv = self.sock.recv(1024).decode()
-            if not recv:
+            data = self.sock.recv(1024).decode()
+            if not data:
                 break
-            response += recv 
+            response += data
         return response
 
+
+    def ger_error_description(data):
+        
+    
     
     def close(self):
         self.sock.close()
+
+
+class HTTPRequest:
+    
+    def __init__(method, path, header, body):
+        self.method = method
+        self.path = path
+        self.header = header
+        self.body = body
+
+
+    def create_request(self):
+        request = "$self.method $self.path HTTP/1.1\r\n"
+        request += "$self.header\r\n"
+        request += "$self.body\r\n"
+        return request
 
 
 class ClientRequestCustomHeader:
@@ -44,6 +81,27 @@ class ClientRequestCustomHeader:
         self.accept = accept
         self.content_length = content_length
         self.accept_language = accept_language
+    
+    
+    def create_header(self):
+        header = ""
+        header += "Host: $self.host\r\n"
+        header += "Accept: $self.accept\r\n"
+        header += "Accept-Language: $self.accept_language\r\n"
+        header += "Content-Length: $self.content_length\r\n"
+        return header
+       
+       
+class HTTPBody:
+      
+    @classmethod
+    def get_from_commandline(cls, args):
+        return cls(args.body)
+        
+    @classmethod
+    def load_from_file(cls, path):
+        return(open(path, 'r').read())
+        
 
 def main():
     server_address = ServerAdress()
